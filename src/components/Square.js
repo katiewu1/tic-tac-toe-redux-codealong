fdefd1a2e25e35482ce810ca55e3b762d92617ee
katiewu1 <83988618+katiewu1@users.noreply.github.com>
2021-11-15 01:00:20 +0100

    codealong

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,16 +1,26 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { game } from 'reducers/game'
 import { Player } from 'components/Player'
 
 export const Square = ({ value, index }) => {
+  const dispatch = useDispatch()
+
   const handleClick = () => {
     // TODO send the captureSquare action
+    // dispatching this action by invoking this function called captureSquare
+    // with index - tell Redux which square user has clicked on.
+    // index - it's a payload, we send an object with a key of index & value of whatever number it's
+    // shorthand: "index", instead of: "index: index"
+    dispatch(game.actions.captureSquare({ index }))
   }
 
   return (
     <button
       className={value === null ? 'square clickable' : 'square'}
-      type="button"
-      onClick={handleClick}>
+      type='button'
+      onClick={handleClick}
+    >
       <Player value={value} />
     </button>
   )
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,33 +1,42 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { game } from 'reducers/game'
 import { Player } from 'components/Player'
 
 export const Stats = () => {
-  // TODO - get the 'winner' from the game reducer
-  const winner = null
-  // TODO - get the 'nextPlayer' from the game reducer
-  const nextPlayer = 'x'
+  const dispatch = useDispatch()
+
+  // TODO - get the 'winner' from the game reducer - use selectors
+  const winner = useSelector((store) => store.game.winner)
+  // TODO - get the 'nextPlayer' from the game reducer - use selectors
+  const nextPlayer = useSelector((store) => store.game.player)
 
   const handleButtonPress = () => {
-    // TODO - dispatch the restart action
+    // TODO - dispatch the restart action by invoking the function restart without any payload
+    dispatch(game.actions.restart())
   }
 
   if (winner) {
     return (
-      <div className="stats">
+      <div className='stats'>
         <p>
           <Player value={winner} /> wins!!!!
         </p>
-        <button type="button" onClick={handleButtonPress}>Start new game</button>
+        <button type='button' onClick={handleButtonPress}>
+          Start new game
+        </button>
       </div>
     )
   }
 
   return (
-    <div className="stats">
+    <div className='stats'>
       <p>
         Next player <Player value={nextPlayer} />
       </p>
-      <button type="button" onClick={handleButtonPress}>Restart</button>
+      <button type='button' onClick={handleButtonPress}>
+        Restart
+      </button>
     </div>
   )
 }
